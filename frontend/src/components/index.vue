8f88f0bbee8cb85a40b6b35bb634f5414b25e1e3
Julien Valois <valois.julien0@gmail.com>
2022-05-18 13:39:05 +0200

    ~page accueil

--- frontend/src/components/index.vue
+++ frontend/src/components/index.vue
@@ -11,9 +11,9 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+      Vous êtes sur le site interne de Groupomania vous pouvez vous
+      <a target="_blank" href="../render/signup.vue">inscrire</a> ou vous
+      <a target="_blank" href="/login">connecter</a>.
     </h3>
   </div>
 </template>
