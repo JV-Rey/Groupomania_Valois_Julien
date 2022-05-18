4a8304a6877e82ed211c71904cdb2813414bd689
Julien Valois <valois.julien0@gmail.com>
2022-05-18 13:39:38 +0200

    template login/signup

---
+++ frontend/src/components/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <form alt="formulaire de connection à Groupomania">
+    <div>
+      <label for="email">Votre adresse mail :
+        <input type="text" id="email" placeholder="Mon email - ex : dupont@groupomania.fr" alt="renseigner votre email">
+      </label>
+
+      <label for="password">Votre mot de passe : 
+        <input type="password" id="password" placeholder="Mon mot de passe* - ex : dupontpass!" alt="choisir votre mot de passe">
+      </label>
+
+      <button type="submit">connection</button>
+    </div>
+  </form>
+</template>
---
+++ frontend/src/components/signup.vue
@@ -0,0 +1,23 @@
+<template>
+  <form alt="formulaire d'inscription à Groupomania">
+    <div>
+      <label for="firstName">Votre prénom :
+        <input type="text" id="firstName" placeholder="Mon prénom - ex : Jean" alt="renseigner votre prénom">
+      </label>
+
+      <label for="lastName">Votre nom :
+        <input type="text" id="lastName"  placeholder="Mon nom - ex : Dupont" alt="renseigner votre nom">
+      </label>
+
+      <label for="email">Votre adresse mail :
+        <input type="text" id="email" placeholder="Mon email - ex : dupont@groupomania.fr" alt="renseigner votre email">
+      </label>
+
+      <label for="password">Votre mot de passe : 
+        <input type="password" id="password" placeholder="Mon mot de passe* - ex : dupontpass!" alt="choisir votre mot de passe">
+      </label>
+
+      <button type="submit">Créer un compte</button>
+    </div>
+  </form>
+</template>
