04cddee9e916ba535b86d3183a80bfcec3df1740
Julien Valois <valois.julien0@gmail.com>
2022-06-08 14:00:28 +0200

    begin on fetchdata

--- frontend/src/view/LoginView.vue
+++ frontend/src/view/LoginView.vue
@@ -1,13 +1,13 @@
 <template>
   <h3>Connection:</h3>
-  <form alt="formulaire de connection à Groupomania">
+  <form @submit.prevent="onSubmit" alt="formulaire de connection à Groupomania">
     <div>
       <label for="email">Votre adresse mail :
-        <input type="text" id="email" placeholder="Mon email - ex : dupont@groupomania.fr" alt="renseigner votre email">
+        <input type="text" id="email" required v-model="email" placeholder="Mon email - ex : dupont@groupomania.fr" alt="renseigner votre email">
       </label>
 
       <label for="password">Votre mot de passe : 
-        <input type="password" id="password" placeholder="Mon mot de passe* - ex : dupontpass!" alt="choisir votre mot de passe">
+        <input type="password" id="password" required v-model="password" placeholder="Mon mot de passe* - ex : dupontpass!" alt="choisir votre mot de passe">
       </label>
 
       <button type="submit">Connection</button>
@@ -15,3 +15,23 @@
   </form>
   <p>Si vous n'avez pas de compte, vous pouvez en <a href="/signup">créer</a> un!</p>
 </template>
+
+<script>
+  export default {
+    name: 'loginView',
+    data(){
+      return{
+        email:"",
+        password:"",
+        id:""
+      }
+    },
+    created() {
+      fetch('http://localhost:3000/api/auth')
+        .then(res => res.json())
+        
+    },
+    
+    
+  }
+</script>
--- frontend/src/view/SignUpView.vue
+++ frontend/src/view/SignUpView.vue
@@ -1,21 +1,21 @@
 <template>
   <h3>Remplicez ces informations pour vous inscrire à Groupomania</h3>
-  <form alt="formulaire d'inscription à Groupomania">
+  <form @submit.prevent="SignUpView" alt="formulaire d'inscription à Groupomania">
     <div>
       <label for="firstName">Votre prénom :
-        <input type="text" id="firstName" placeholder="Mon prénom - ex : Jean" alt="renseigner votre prénom">
+        <input type="text" id="firstName" v-model="firstName" placeholder="Mon prénom - ex : Jean" alt="renseigner votre prénom">
       </label>
 
       <label for="lastName">Votre nom :
-        <input type="text" id="lastName"  placeholder="Mon nom - ex : Dupont" alt="renseigner votre nom">
+        <input type="text" id="lastName" v-model="lastName"  placeholder="Mon nom - ex : Dupont" alt="renseigner votre nom">
       </label>
 
       <label for="email">Votre adresse mail :
-        <input type="text" id="email" placeholder="Mon email - ex : dupont@groupomania.fr" alt="renseigner votre email">
+        <input type="text" id="email" v-model="email" placeholder="Mon email - ex : dupont@groupomania.fr" alt="renseigner votre email">
       </label>
 
       <label for="password">Votre mot de passe : 
-        <input type="password" id="password" placeholder="Mon mot de passe* - ex : dupontpass!" alt="choisir votre mot de passe">
+        <input type="password" id="password" v-model="password" placeholder="Mon mot de passe* - ex : dupontpass!" alt="choisir votre mot de passe">
       </label>
 
       <button type="submit">Créer un compte</button>
@@ -25,16 +25,27 @@
 </template>
 
 <script>
+import axios from 'axios'
   export default {
-    name: 'SignUp',
+    name: 'SignUpView',
     data(){
       return{
-        firstName:"",
-        lastName:"",
-        email:"",
-        password:""
-      }
-    }
+        FormData: {
+          firstName:"",
+          lastName:"",
+          email:"",
+          password:""
+        }
 
+      }
+    },
+    
+    methods: {
+      signUp(){
+        axios.post('http://localhost:3000/api/user', this.formData)
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err))
+      }
+    },
   }
 </script>
