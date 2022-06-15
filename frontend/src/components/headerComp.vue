52fde7153df08568cf87ffea06fb2b1e52ad78fa
Julien Valois <valois.julien0@gmail.com>
2022-06-15 12:55:22 +0200

    begin header

---
+++ frontend/src/components/headerComp.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <img src="../assets/icon-left-font.svg" alt="Logo Groupomania">
+        <a href="/">Acceuil</a>
+        <a href="/newpost">Creer un post</a>
+        <a href="/signup">Déconnection</a>
+    </div>
+</template>
